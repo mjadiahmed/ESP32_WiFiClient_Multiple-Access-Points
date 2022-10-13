d7595d78e94e5537ab49a2740d4ca55edaa8fd88
Ahmed MJADI <92577300+mjadiahmed@users.noreply.github.com>
2022-10-13 13:05:13 +0100

    Works well !

---
+++ src/main.cpp
@@ -0,0 +1,46 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiMulti.h>
+
+WiFiMulti wifiMulti;
+
+void setup()
+{
+  Serial.begin(9600);
+  delay(10);
+
+  wifiMulti.addAP("NEXT-AP1", "2899100*-+");
+  wifiMulti.addAP("NEXT-AP1", "Next2022");
+  wifiMulti.addAP("NEXTRONIC-1", "Next2021");
+
+  Serial.println("Connecting Wifi...");
+
+  if (wifiMulti.run() == WL_CONNECTED)
+  {
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+  }
+}
+
+void loop()
+{
+  if (wifiMulti.run() != WL_CONNECTED)
+  {
+    Serial.println("WiFi not connected!");
+    delay(1000);
+  }
+  // Serial.println(WiFi.localIP());
+
+  Serial.print("WiFi SSID: ");
+  Serial.print(WiFi.SSID());
+  Serial.print("   ||   AP_MAC: ");
+  Serial.print(WiFi.BSSIDstr());
+  Serial.print("  ||   RSSI: ");
+  Serial.print(WiFi.RSSI());
+  Serial.print("  ||   Channel: ");
+  Serial.println(WiFi.channel());
+
+  delay(1000);
+}
