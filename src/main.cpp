6035af22d0dc343e0fd44ebc2a00033433a3580c
Ahmed MJADI <92577300+mjadiahmed@users.noreply.github.com>
2023-01-17 17:03:03 +0100

    add table and loop of adding AP

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,8 @@
  * @file main.cpp
  * @author Ahmed MJADI (mjadiahmed@gmail.com)
  * @brief Connect your ESP32 to the Accesspoint with the best signal
- * @version 0.1
- * @date 2022-10-13
+ * @version 0.2
+ * @date 2022-01-17
  * @copyright Copyright (c) 2022
  */
 
@@ -12,15 +12,23 @@
 #include <WiFiMulti.h>
 
 WiFiMulti wifiMulti;
+String WiFiAP[6] = {"NXT-AP2", "2899100*-+", "NEXT-AP1", "2899100*-+", "NEXTRONIC-1", "Next2021"};
 
 void setup()
 {
   Serial.begin(9600);
   delay(10);
 
-  wifiMulti.addAP("NEXT-AP1", "2899100*-+");
-  wifiMulti.addAP("NEXT-AP1", "Next2022");
-  wifiMulti.addAP("NEXTRONIC-1", "Next2021");
+  // wifiMulti.addAP("NXT-AP2", "2899100*-+");
+  // wifiMulti.addAP("NEXT-AP1", "2899100*-+");
+  // wifiMulti.addAP("NEXTRONIC-1", "Next2021");
+  /*setup  multi WiFi*/
+  for (int i = 0; i < 5; i = i + 2)
+  {
+    Serial.println("[D] Wifi AP table #  " + String(WiFiAP[i].c_str()) + " | " + String(WiFiAP[i + 1].c_str()));
+    wifiMulti.addAP(WiFiAP[i].c_str(), WiFiAP[i + 1].c_str());
+    delay(1);
+  }
 
   Serial.println("Connecting Wifi...");
 
